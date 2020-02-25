edfaa819329a89afb404332409ee44ee1abeca45
mingz <305603665@qq.com>
2020-02-25 21:47:04 +0800

    chess

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package chinese_chess_table_go
 
-func main() {
+func getActionList() (actions []*action) {
+	return
+}
+
+func Main() {
+
+	actionList := getActionList()
+
 	b := NewChessBoard()
-	b.DoAction()
+
+	for _, action := range actionList {
+		ok := b.DoAction(action)
+		if !ok {
+			break
+		}
+	}
 }
