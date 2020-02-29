0158f81f740e1696bf2b662c31a70bfa6128050d
mingz <305603665@qq.com>
2020-02-29 13:26:15 +0800

    chess

--- board/action.go
+++ board/action.go
@@ -10,8 +10,8 @@ type Action struct {
 func (a Action) getInfo() {
 
 }
-func (c Action) String() string {
-	return fmt.Sprintf("Action(%v, %v)", c.Src, c.Dst)
+func (a Action) String() string {
+	return fmt.Sprintf("Action(%v, %v)", a.Src, a.Dst)
 }
 
 func NewAction(src, dst Point) Action {
